0cb3c657ba283a2da95c9c0b1098c4eaa3b83df2
Rob Nelson <guruvan@users.noreply.github.com>
2018-08-22 14:20:20 -0700

    add dev prod case

--- script/ci-deploy.sh
+++ script/ci-deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+STAGE=$1
 CID=$2
-docker exec $CID ls -la /data/ipfs
-docker exec $CID whoami 
+
+case ${STAGE} in 
+   dev) docker exec $CID ls -la /data/ipfs
+        docker exec $CID ls -la /data/
+        ;;
+  prod) docker exec $CID whoami 
+        ;;
+     *) echo "unknown error"
+        exit 1
+        ;;
+esac
+
+
