8f5dfe271ab70e45217c107936308afbb21d96c7
Rob Nelson <guruvan@maza.club>
2018-08-22 17:55:20 -0700

    just use docker --name already

--- script/ci-deploy.sh
+++ script/ci-deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 STAGE=$1
-CID=$2
 
 case ${STAGE} in 
-   dev) docker exec $CID /data/script/ipfs-add-publish.sh dev
+   dev) docker exec ipfs /data/script/ipfs-add-publish.sh dev
         ;;
-  prod) docker exec $CID /data/script/ipfs-add-publish.sh prod
+  prod) docker exec ipfs /data/script/ipfs-add-publish.sh prod
         ;;
      *) echo "unknown error"
         exit 1
